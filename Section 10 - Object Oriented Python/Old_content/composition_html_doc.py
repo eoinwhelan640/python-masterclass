ca25064029740b7666562989f8e6d92bd899d66e
Eoin Whelan <whelan.eoin6@gmail.com>
2022-07-19 15:35:07 -0700

    Check out repo on old laptop + rearrange files

--- Section 10 - Object Oriented Python/Old_content/composition_html_doc.py
+++ Section 10 - Object Oriented Python/Old_content/composition_html_doc.py
@@ -83,5 +83,5 @@ if __name__ == '__main__':
     my_page.add_tag("h2", "sub-heading")
     my_page.add_tag("p", "This is a paragraph that will appear on the page")
     # the display function calls the print of each title, head & body
-    with open('test.html','w',encoding='utf-8') as write_file:
+    with open('test.html', 'w', encoding='utf-8') as write_file:
         my_page.display(file=write_file)
