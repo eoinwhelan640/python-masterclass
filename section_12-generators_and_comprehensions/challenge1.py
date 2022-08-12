0a38a7e10aa632c4cdfe2367a1a75dba09694b30
Eoin Whelan <whelan.eoin6@gmail.com>
2022-08-12 14:32:21 -0700

    Start of comprehension files

---
+++ section_12-generators_and_comprehensions/challenge1.py
@@ -0,0 +1,28 @@
+# Rewrite the following code to use a list comprehension, instead of a for loop.
+#
+# Add your solution below the loop, so that the resulting list is printed out
+# below output - that makes it easier to check that it's producing exactly
+# the same list (and avoids entering the input text twice).
+ 
+text = input("Please enter your text: ")
+ 
+output = []
+[output.append(len(x)) for x in text.split()]
+#for x in text.split():
+#    output.append(len(x))
+print(output)
+ 
+# type your solution here:
+ 
+ 
+# It could be useful to store the original words in the list, as well.
+# The for loop would look like this (note the extra parentheses, so
+# that we get tuples in the list):
+ 
+output = []
+for x in text.split():
+    output.append((x, len(x)))
+print(output)
+ 
+# type the corresponding comprehension here:
+[output.append((x,len(x))) for x in text.split()]
